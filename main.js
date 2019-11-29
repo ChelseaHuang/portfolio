6cceccb861b961324b74f00702a3a2f1c4308259
ChelseaHuang <yh3167@tc.columbi.edu>
2019-11-28 21:57:55 -0500

    Portofolio Draft

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ const navSlide = () => {
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-  })
+  });
+
+
+  navLink.forEach((link,index) => {
+    link.style.animation = 'navLinkFade 0.5s ease fowards ${index / 7}s';
+  });
+
 }
-
-
 
 navSlide();
