6e893ff518183c780a1a8397ee81b4538ec3cd73
ChelseaHuang <yh3167@tc.columbi.edu>
2019-12-09 22:39:17 -0500

    portfolio 12.9

--- main.js
+++ main.js
@@ -2,11 +2,41 @@ const navSlide = () => {
   const burger = document.querySelector ('.burger');
   const nav = document.querySelector('.nav-link');
   const navLink = document.querySelectorAll('.nav-link li');
+  const box1 = document.querySelector('.box1');
+  const box2 = document.querySelector('.box2');
+  const intro = document.querySelector('.intro');
+  const skills= document.querySelector('.skills');
+
 
   burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+    $(".projects").toggleClass('hide');
+    $(".navbar").toggleClass('show');
   });
 
+
+box1.addEventListener('mouseover',function(event){
+  $(".box1").toggleClass("opacity");
+  $(".intro").toggleClass("show");
+  $(".skills").toggleClass("show");
+})
+
+box1.addEventListener('mouseout',function(event){
+  $(".box1").toggleClass("opacity");
+  $(".intro").toggleClass("show");
+  $(".skills").toggleClass("show");
+})
+
+box2.addEventListener('mouseover',function(event){
+  $(".box2").toggleClass("opacity");
+  $(".intro").toggleClass("show");
+  $(".skills").toggleClass("show");
+})
+
+box2.addEventListener('mouseout',function(event){
+  $(".box2").toggleClass("opacity");
+  $(".intro").toggleClass("show");
+  $(".skills").toggleClass("show");
+})
 
   navLink.forEach((link,index) => {
     link.style.animation = 'navLinkFade 0.5s ease fowards ${index / 7}s';
