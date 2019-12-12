e2e2fab66d1dedf2e6c5a835e0254ee9cedf8d8b
ChelseaHuang <yh3167@tc.columbi.edu>
2019-12-12 13:13:09 -0500

    portfolio 12.12

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const navSlide = () => {
   const box1 = document.querySelector('.box1');
   const box2 = document.querySelector('.box2');
   const intro = document.querySelector('.intro');
+  const intro2 = document.querySelector('.intro2');
   const skills= document.querySelector('.skills');
 
 
@@ -14,34 +15,39 @@ const navSlide = () => {
   });
 
 
-box1.addEventListener('mouseover',function(event){
-  $(".box1").toggleClass("opacity");
-  $(".intro").toggleClass("show");
-  $(".skills").toggleClass("show");
-})
-
-box1.addEventListener('mouseout',function(event){
-  $(".box1").toggleClass("opacity");
-  $(".intro").toggleClass("show");
-  $(".skills").toggleClass("show");
-})
-
-box2.addEventListener('mouseover',function(event){
-  $(".box2").toggleClass("opacity");
-  $(".intro").toggleClass("show");
-  $(".skills").toggleClass("show");
-})
-
-box2.addEventListener('mouseout',function(event){
-  $(".box2").toggleClass("opacity");
-  $(".intro").toggleClass("show");
-  $(".skills").toggleClass("show");
-})
-
-  navLink.forEach((link,index) => {
-    link.style.animation = 'navLinkFade 0.5s ease fowards ${index / 7}s';
-  });
 
 }
 
 navSlide();
+
+document.querySelector(".box1").addEventListener("mouseover", mouseOver);
+document.querySelector(".box1").addEventListener("mouseout", mouseOut);
+
+
+function mouseOver() {
+  document.querySelector(".intro").style.display = "block";
+  // console.log("in");
+}
+
+function mouseOut() {
+  document.querySelector(".intro").style.display = "none";
+}
+
+  // console.log("out");
+
+  // document.querySelector(".box2").addEventListener("mouseover", mouseOver);
+// document.querySelector(".box2").addEventListener("mouseout", mouseOut);
+//
+// function mouseOver() {
+//   document.querySelector(".intro2").style.display = "block";
+//   // console.log("in");
+// }
+//
+// function mouseOut() {
+//   document.querySelector(".intro2").style.display = "none";
+//   // console.log("out");
+// }
+
+navLink.forEach((link,index) => {
+  link.style.animation = 'navLinkFade 0.5s ease fowards ${index / 7}s';
+});
